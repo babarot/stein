6ae1a167b172d5adc3cd42e9c2c9f124e527914e
b4b4r07 <b4b4r07@gmail.com>
2019-08-27 00:11:03 +0900

    GJSON handle parsed arg as just string everytime

--- lint/internal/policy/funcs/jsonpath.go
+++ lint/internal/policy/funcs/jsonpath.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
-	ctyjson "github.com/zclconf/go-cty/cty/json"
 )
 
 // JSONPathFunc is
@@ -117,38 +116,15 @@ func GJSONFunc(file string, data []byte) function.Function {
 			Type: cty.DynamicPseudoType,
 		},
 		Type: func(args []cty.Value) (cty.Type, error) {
-			query := args[0].AsString()
-			defaultVal := cty.StringVal("")
-			if len(args) > 1 {
-				defaultVal = args[1]
-			}
-			res, err := getJSON(query, file, data)
-			if err != nil {
-				return defaultVal.Type(), nil
-			}
-			ty, err := ctyjson.ImpliedType(res)
-			if err != nil {
-				// When the result from getJSON can not be converted to JSON (that is, array or map),
-				// treat the return value as a string
-				return defaultVal.Type(), nil
-			}
-			return ty, nil
+			return cty.String, nil
 		},
 		Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
 			query := args[0].AsString()
-			defaultVal := cty.StringVal("")
-			if len(args) > 1 {
-				defaultVal = args[1]
+			result := gjson.GetBytes(data, query)
+			if !result.Exists() {
+				return cty.StringVal(""), nil
 			}
-			res, err := getJSON(query, file, data)
-			if err != nil {
-				return defaultVal, nil
-			}
-			val, err := ctyjson.Unmarshal(res, retType)
-			if err != nil {
-				return cty.StringVal(string(res)), nil
-			}
-			return val, nil
+			return cty.StringVal(result.String()), nil
 		},
 	})
 }
